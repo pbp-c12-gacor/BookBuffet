6c2fb618280804d3d9900cd3a1e7a386b34bde6c
fionafrm <fiona.ratu@ui.ac.id>
2023-10-25 11:39:14 +0700

    report: add report models

--- report/models.py
+++ report/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from main.models import Book, User
 
 # Create your models here.
+class Report(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    book = models.ForeignKey(Book)
+    comment = models.TextField()
+    date_added = models.DateTimeField(auto_now_add=True)
