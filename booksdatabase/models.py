4e0c9734aa45814c95c4667e655c0d55d38d5c42
Muhammad Andhika Prasetya <andhikapraa@gmail.com>
2023-10-25 15:36:06 +0700

    Create: Models for booksdatabase

--- booksdatabase/models.py
+++ booksdatabase/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
 # Create your models here.
+class Book(models.Model):
+    title = models.CharField(max_length=255)
+    subtitle = models.TextField(null=True, blank=True)
+    authors = models.ManyToManyField("Author", related_name="books")
+    publisher = models.CharField(max_length=255)
+    published_date = models.DateField()
+    description = models.TextField(null=True, blank=True)
+    page_count = models.IntegerField()
+    categories = models.ManyToManyField("Category", related_name="books")
+    language = models.CharField(max_length=255)
+    preview_link = models.URLField()
+    cover_link = models.URLField(null=True, blank=True)
+    industry_identifiers = models.JSONField()
+
+
+class Author(models.Model):
+    name = models.CharField(max_length=255)
+    book = models.ForeignKey(Book, on_delete=models.CASCADE, related_name="authors")
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=255)
+    book = models.ForeignKey(Book, on_delete=models.CASCADE, related_name="categories")
