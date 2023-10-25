e1594661fda42a605a1a7c1b19365f3bb9544078
Muhammad Andhika Prasetya <andhikapraa@gmail.com>
2023-10-25 16:42:58 +0700

    Modify: add classes to view books in the database

--- booksdatabase/views.py
+++ booksdatabase/views.py
@@ -1,3 +1,36 @@
-from django.shortcuts import render
+from rest_framework import viewsets, filters, generics
+from .models import Book, Author, Category
+from .serializers import BookSerializer, AuthorSerializer, CategorySerializer
+from .permissions import IsAdminOrReadOnly
 
-# Create your views here.
+
+class BookViewSet(viewsets.ModelViewSet):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    permission_classes = [IsAdminOrReadOnly]
+
+
+class AuthorViewSet(viewsets.ModelViewSet):
+    queryset = Author.objects.all()
+    serializer_class = AuthorSerializer
+    permission_classes = [IsAdminOrReadOnly]
+
+
+class CategoryViewSet(viewsets.ModelViewSet):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+    permission_classes = [IsAdminOrReadOnly]
+
+
+class BookList(generics.ListAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    filter_backends = [filters.SearchFilter]
+    search_fields = ["title", "authors__name", "categories__name"]
+
+
+class BookDetail(generics.RetrieveAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    lookup_field = "pk"
+    lookup_url_kwarg = "book_id"
