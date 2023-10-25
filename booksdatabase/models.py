8a56559bb271d170c31fd0f0173482f2fb574331
Muhammad Andhika Prasetya <andhikapraa@gmail.com>
2023-10-25 16:41:42 +0700

    Modify: Fix bugs on naming

--- booksdatabase/models.py
+++ booksdatabase/models.py
@@ -1,15 +1,22 @@
+"""
+This module contains the models for the Book Buffet application.
+"""
+
 from django.db import models
 
-# Create your models here.
+
 class Book(models.Model):
+    """
+    A model representing a book.
+    """
     title = models.CharField(max_length=255)
     subtitle = models.TextField(null=True, blank=True)
-    authors = models.ManyToManyField("Author", related_name="books")
+    authors = models.ManyToManyField("Author", related_name="books_authored")
     publisher = models.CharField(max_length=255)
     published_date = models.DateField()
     description = models.TextField(null=True, blank=True)
     page_count = models.IntegerField()
-    categories = models.ManyToManyField("Category", related_name="books")
+    categories = models.ManyToManyField("Category", related_name="books_in_category")
     language = models.CharField(max_length=255)
     preview_link = models.URLField()
     cover_link = models.URLField(null=True, blank=True)
@@ -17,10 +24,16 @@ class Book(models.Model):
 
 
 class Author(models.Model):
+    """
+    A model representing an author.
+    """
     name = models.CharField(max_length=255)
-    book = models.ForeignKey(Book, on_delete=models.CASCADE, related_name="authors")
+    book = models.ForeignKey(Book, on_delete=models.CASCADE, related_name="authors_books")
 
 
 class Category(models.Model):
+    """
+    A model representing a category.
+    """
     name = models.CharField(max_length=255)
-    book = models.ForeignKey(Book, on_delete=models.CASCADE, related_name="categories")
+    book = models.ForeignKey(Book, on_delete=models.CASCADE, related_name="categories_books")
