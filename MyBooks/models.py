6253c10613368c159aa1e4898f4afe212f06c287
Faishal Nelwan <108632813+pesolosep@users.noreply.github.com>
2023-10-26 18:41:18 +0700

    fix html display

--- MyBooks/models.py
+++ MyBooks/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from main.models import *
+from booksdatabase.models import *
 
 # Create your models here.
+class MyBook(models.model):
+    User = models.ForeignKey(User, on_delete= models.CASCADE)
+    Book = models.ForeignKey(Book, on_delete= models.CASCADE)
+    date_added = models.DateField(auto_now_add=True)
+    review = models.TextField()
