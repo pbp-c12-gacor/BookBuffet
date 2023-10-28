48d7b8b9bc559187ff4d50ee41fb05ee8e65de2b
fionafrm <fiona.ratu@ui.ac.id>
2023-10-29 01:25:37 +0700

    Add form report and show_report

--- report/forms.py
+++ report/forms.py
@@ -1,11 +1,26 @@
-from django.forms import ModelForm, TextInput, Textarea
+from django.forms import ModelChoiceField, ModelForm, Select, Textarea, TextInput, ValidationError, CharField
 from report.models import Report
+from booksdatabase.models import Book
 
 class ReportForm(ModelForm):
     class Meta:
         model = Report
-        fields = ['book', 'comment']
-        widgets = {
-            'book': TextInput(attrs={'id': 'book_title'}),
-            'comment': Textarea(attrs={'placeholder': 'Enter comment'}),
-        }
+        fields = ['book_title', 'comment']
+    
+    book_title = CharField(
+        widget=TextInput(attrs={'placeholder': 'Your Book Title'}),
+        required= True
+    )
+    comment = CharField(
+        widget=Textarea(attrs={'placeholder': 'Your Comment'}),
+        required= True
+    )
+
+    def clean(self):
+        cleaned_data = super().clean()
+        book = Book.objects.get(title=cleaned_data.get('book_title'))
+        comment = cleaned_data.get('comment')
+
+        # Add custom validation for book and comment
+        if not book or not comment:
+            raise ValidationError("Both book and comment are required.")
