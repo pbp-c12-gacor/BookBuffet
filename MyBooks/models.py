f696ad29ed08e386144e4df4d545ef2f0d817c90
Faishal Nelwan <108632813+pesolosep@users.noreply.github.com>
2023-10-29 20:06:27 +0700

    finish MyBooks module

--- MyBooks/models.py
+++ MyBooks/models.py
@@ -3,7 +3,6 @@ from main.models import *
 from booksdatabase.models import *
 
 # Create your models here.
-
 class MyBook(models.Model):
     user = models.ForeignKey(User, on_delete= models.CASCADE)
     date_added = models.DateField(auto_now_add=True,  null=True, blank=True)
@@ -11,6 +10,7 @@ class MyBook(models.Model):
 
 class Review(models.Model):
     user = models.ForeignKey(User, on_delete= models.CASCADE,  null=True, blank=True)
+    username = models.CharField(default="", max_length=50)
     book = models.ForeignKey(Book, on_delete=models.CASCADE, null=True, blank=True)
     review = models.TextField(default = "")
     rating = models.IntegerField(default =0)
