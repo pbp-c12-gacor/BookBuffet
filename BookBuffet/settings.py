871d56420d45664745f88438d97d0dda45024011
Muhammad Andhika Prasetya <44688573+andhikapraa@users.noreply.github.com>
2023-12-01 13:49:59 +0000

    Remove whitenoise middleware

--- BookBuffet/settings.py
+++ BookBuffet/settings.py
@@ -63,7 +63,6 @@ MIDDLEWARE = [
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
     "corsheaders.middleware.CorsMiddleware",
-    "whitenoise.middleware.WhiteNoiseMiddleware",
 ]
 
 CORS_ALLOW_ALL_ORIGINS = True
