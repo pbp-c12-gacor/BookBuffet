b30fb12f0b13485d8af8b6566264659be0efe02d
Muhammad Andhika Prasetya <44688573+andhikapraa@users.noreply.github.com>
2023-12-01 13:10:23 +0000

    Test Render Deploy

--- BookBuffet/settings.py
+++ BookBuffet/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 import os
 import environ
+import dj_database_url
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -22,7 +23,7 @@ env = environ.Env()
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-$l0l5c@lt^$idrya-doni%sh^zlin#u&ap40_!8$a8nk+!#l5o"
+SECRET_KEY = os.environ.get('SECRET_KEY', default='django-insecure-$l0l5c@lt^$idrya-doni%sh^zlin#u&ap40_!8$a8nk+!#l5o')
 
 PRODUCTION = env.bool("PRODUCTION", default=False)
 
@@ -62,6 +63,7 @@ MIDDLEWARE = [
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
     "corsheaders.middleware.CorsMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
 ]
 
 CORS_ALLOW_ALL_ORIGINS = True
@@ -96,17 +98,17 @@ WSGI_APPLICATION = "BookBuffet.wsgi.application"
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
+    "default": dj_database_url.config(
+        default=env("DATABASE_URL", default="sqlite:///db.sqlite3"),
+        conn_max_age=600,
+    )
 }
 
-if PRODUCTION:
-    DATABASES = {
-        'default': env.db('DATABASE_URL')
-    }
-    DATABASES["default"]["ATOMIC_REQUESTS"] = True
+# if PRODUCTION:
+#     DATABASES = {
+#         'default': env.db('DATABASE_URL')
+#     }
+#     DATABASES["default"]["ATOMIC_REQUESTS"] = True
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
