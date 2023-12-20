0dde950a382d6e278876a1345c42bad630812065
fionafrm <fiona.ratu@ui.ac.id>
2023-12-20 18:44:11 +0700

    Edit report urls

--- report/urls.py
+++ report/urls.py
@@ -12,5 +12,5 @@ urlpatterns = [
     path('json/', show_report_json, name='show_report_json'),
     path('get-user/<int:user_id>/', get_user_by_id, name='get_user_by_id'),
     path('create-report-flutter/', create_report_flutter, name='create_report_flutter'),
-    path('delete-report-flutter/<int:id>', delete_report_flutter, name='delete_report_flutter'),
+    path('delete-report-flutter/<int:id>/', delete_report_flutter, name='delete_report_flutter'),
 ]
